19a8589f99b79faccc0c8c03d58861dddaf0a466
HamdanAshimi <ashimiademola4@gmail.com>
2026-07-24 02:59:54 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
+import Todo from "./models/todo";
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const onAddTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+
+    setTodos((prevTodos) => prevTodos.concat(newTodo));
+  };
+
+  const removeTodoHandler = (todoId: string) =>
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== todoId));
+
+  return (
+    <div>
+      <NewTodo onAddTodo={onAddTodoHandler} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
+    </div>
+  );
+}
+
+export default App;
