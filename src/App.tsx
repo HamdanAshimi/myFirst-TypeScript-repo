c7ac28bee3caea6b761d236cf44aeb7ad118bfbf
HamdanAshimi <ashimiademola4@gmail.com>
2026-07-25 08:49:20 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,8 @@
 import { useState } from "react";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  const onAddTodoHandler = (todoText: string) => {
-    const newTodo = new Todo(todoText);
-
-    setTodos((prevTodos) => prevTodos.concat(newTodo));
-  };
-
-  const removeTodoHandler = (todoId: string) =>
-    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== todoId));
-
   return (
     <div>
       <NewTodo onAddTodo={onAddTodoHandler} />
