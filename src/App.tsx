fe670f31682833dec28523139ae1b0fb87797780
HamdanAshimi <ashimiademola4@gmail.com>
2026-07-25 09:26:00 +0100

    Edited App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
+import TodosContextProvider from "./store/todos-context";
 
 function App() {
   return (
     <div>
-      <NewTodo onAddTodo={onAddTodoHandler} />
-      <Todos items={todos} onRemoveTodo={removeTodoHandler} />
+      <TodosContextProvider>
+        {" "}
+        <NewTodo />
+        <Todos />
+      </TodosContextProvider>
     </div>
   );
 }
